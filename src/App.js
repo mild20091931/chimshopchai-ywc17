f1921b88a3ea0b472a497e03b2f44a0c48cfa613
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-26 21:38:43 +0700

    [Add] App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Index from '../src/components/Core/App'
 
 function App() {
   return (
-    <div className="App">
-      {process.env.REACT_APP_NOT_SECRET_CODE}
-    </div>
+    <Index/>
   );
 }
 
