0fc6162fb47a1c765be2ae0b2b179691eb8ee6c3
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 12:15:42 +0700

    [Add] index.js

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component,Fragment } from 'react';
+import Nav from '../Core/Nav'
+
+class Index extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Nav/>
+      </Fragment>
+    );
+  }
+}
+
+export default Index;
