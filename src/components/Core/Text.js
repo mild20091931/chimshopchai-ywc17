982f823a91f83c163dc376d34732e980013f82d2
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 15:36:31 +0700

    [Add] Text.js

---
+++ src/components/Core/Text.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import size from './FontSize'
+
+export const Title = styled.p`
+  font-size: ${size.title};
+  `
+export const SubTitle = styled.p`
+  font-size: ${size.subtitle};
+`
+export const Header = styled.p`
+  font-size: ${size.header};
+`
