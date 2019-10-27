ab46ce0e66f297853f1aafb7b397e9e37a4e7224
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 12:15:55 +0700

    [Add] Banner.js

---
+++ src/components/Core/Banner.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div>
+        Banner
+      </div>
+    );
+  }
+}
+
+export default Banner;
