4103d2b250739b7553cbb4c28d8585ba93023d90
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 11:56:10 +0700

    [Update] sticky-top

--- src/components/Core/Nav.js
+++ src/components/Core/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from 'react';
 import styled from 'styled-components';
-import api from './api';
+import api from './apiService';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const NavIcon = styled.div`
@@ -39,7 +39,7 @@ class NavBar extends Component {
     const { crossNav,navbarItems } = this.state;
     return (
       <React.Fragment>
-        <Navbar className="d-lg-none" color="faded" light>
+        <Navbar className="d-lg-none sticky-top" color="faded" light>
         <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
         <NavIcon onClick={() => this.handleClick(crossNav)}>
             {crossNav ? <h3>X</h3> : <NavbarToggler/>}
@@ -56,7 +56,7 @@ class NavBar extends Component {
           </Nav>
         </Collapse>
       </Navbar>
-      <Navbar color="faded" light expand="md" className="d-none d-lg-block d-xl-block">
+      <Navbar color="faded" light expand="md" className="d-none d-lg-block d-xl-block sticky-top">
         <Nav className="nav justify-content-center" navbar>
           {navbarItems.map((data,i) => (
             <React.Fragment>
