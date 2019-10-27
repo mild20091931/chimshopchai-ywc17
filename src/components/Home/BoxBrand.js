8431f4a10c5d965f7b60e4f904f151d20c03ea8f
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 23:06:30 +0700

    [Add] BixBrand.js

---
+++ src/components/Home/BoxBrand.js
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import styled from 'styled-components';
+import { Container, Row, Col } from 'reactstrap';
+
+const Bg = styled.div`
+  margin: 150px 0px 150px 0px; 
+`
+const Img = styled.img`
+  width: 102px;
+`
+
+const pathImg = [
+  { img: "MOF.png" },
+  { img: "FPO.png" },
+  { img: "CGD.png" },
+  { img: "Krungthai.png" },
+  { img: "MOTS.png" },
+  { img: "TAT.png" },
+]
+const BoxBrand = () => {
+  return (
+    <Bg>
+      <Container fluid={true}>
+        <Row >
+          <Col/>
+          {pathImg.map((data,i)=>{
+            return (
+              <Col key={i} className="d-flex justify-content-center">
+                <Img src={`/img/brand_logo/${data.img}`}/>
+              </Col>
+            )
+          })
+          }
+          <Col/>
+        </Row>
+      </Container>
+    </Bg>
+  )
+}
+
+export default BoxBrand
