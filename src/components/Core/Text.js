9a9dbb75f47589e280248190c4487bec9b373d10
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 16:35:38 +0700

    [Update ] add text style

--- src/components/Core/Text.js
+++ src/components/Core/Text.js
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 import size from './FontSize'
 
-export const Title = styled.p`
+export const Title = styled.h3`
   font-size: ${size.title};
+  color: ${props => props.color};
   `
 export const SubTitle = styled.p`
   font-size: ${size.subtitle};
-`
+  `
 export const Header = styled.p`
   font-size: ${size.header};
+  `
+export const P1 = styled.p`
+  font-size: ${size.p1};
+  `
+export const P2 = styled.p`
+  font-size: ${size.p2};
+`
+export const P3 = styled.p`
+  font-size: ${size.p3};
 `
