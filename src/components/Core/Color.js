ee2c1193219f9696a3ca392c7fc9419abafdf2d2
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 12:11:59 +0700

    [Add] FontSize.js and Color.js

---
+++ src/components/Core/Color.js
@@ -0,0 +1,7 @@
+export default {
+  bule: '#213A8F',
+  yellow: '#FBBC33',
+  headTitle: '#E6332A',
+  font: '#333333',
+  bgFooter: '#FAFAFA',
+}
---
+++ src/components/Core/FontSize.js
@@ -0,0 +1,8 @@
+export default {
+  title: '48px',
+  subtitle: '36px',
+  header: '24px',
+  p1: '18px',
+  p2: '16px',
+  p3: '14px',
+}
