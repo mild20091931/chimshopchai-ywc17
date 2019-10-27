8ab2db95d970459c8dcd5748f49f9d3d2f8282a0
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 19:09:57 +0700

    [Update] edit font-family

--- src/components/Core/Text.js
+++ src/components/Core/Text.js
@@ -2,21 +2,32 @@ import styled from 'styled-components';
 import size from './FontSize'
 
 export const Title = styled.h3`
+  font-family: 'Prompt', sans-serif;
   font-size: ${size.title};
   color: ${props => props.color};
   `
 export const SubTitle = styled.p`
+  font-family: 'Prompt', sans-serif;
+  color: ${props => props.color};
   font-size: ${size.subtitle};
   `
 export const Header = styled.p`
+  font-family: 'Prompt', sans-serif;
+  color: ${props => props.color};
   font-size: ${size.header};
   `
 export const P1 = styled.p`
   font-size: ${size.p1};
+  color: ${props => props.color};
+  font-family: 'Bai Jamjuree', sans-serif;
   `
 export const P2 = styled.p`
   font-size: ${size.p2};
-`
+  color: ${props => props.color};
+  font-family: 'Bai Jamjuree', sans-serif;
+  `
 export const P3 = styled.p`
   font-size: ${size.p3};
+  color: ${props => props.color};
+  font-family: 'Bai Jamjuree', sans-serif;
 `
