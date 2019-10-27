ed86f8e45cf5ea5dcd4132ae7374284d08e0a091
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-27 19:36:27 +0700

    [Add] DetailRight.js and DetailLeft.js

---
+++ src/components/Home/DetailLeft.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Title,SubTitle,P2 } from '../Core/Text';
+
+const DetailLeft = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <SubTitle>ค้นหารายชื่อร้านค้า</SubTitle>
+        </Col>
+        <Col>
+          <img src={'/img/banner_store.png'}/>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default DetailLeft
