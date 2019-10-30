610d06c49f9e292bbe04f65abc3b299f9dd77882
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-30 21:25:46 +0700

    [Update add style button style

--- src/components/Core/Button.js
+++ src/components/Core/Button.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import color from './Color'
+import color from './Color';
+import { Button } from 'reactstrap';
 
 export const ButtonRegister = styled.button`
   width: 50%;
@@ -8,10 +9,27 @@ export const ButtonRegister = styled.button`
   border: 0px;
   color: ${color.white};
   border-radius: 5px;
+  box-shadow: 0 15px 30px 0 rgba(28, 78, 132, 0.4);
   &:hover{
     background-color:${color.yellow};
   }
-  @media screen and (min-width: 325px) and (max-width: 768px) {
+  @media screen and (min-width: 320px) and (max-width: 768px) {
     width: 100%;
+  }
+`
+
+export const PrimaryButton = styled.button`
+  color: ${color.blue};
+  border-radius: 5px;
+  padding: 0.5em;
+  font-family: TATSanaSuksaBold;
+  background-color: ${color.white};
+  border: solid 4px ${color.blue};
+  font-size: 18px;
+  margin-top: 30px;
+  font-weight: bold;
+  &:hover{
+    background-color: ${color.blue};
+    color: ${color.white};
   }
 `
