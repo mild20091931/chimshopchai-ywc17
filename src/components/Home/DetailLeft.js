a9f0ea3c4d537b568ed75f0d674e41aa74156fd6
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-30 21:26:43 +0700

    [Update] style img

--- src/components/Home/DetailLeft.js
+++ src/components/Home/DetailLeft.js
@@ -4,25 +4,42 @@ import styled from 'styled-components';
 import { Container, Row, Col } from 'reactstrap';
 import { SubTitle } from '../Core/Text';
 import MDreact from 'react-markdown/with-html';
+import { PrimaryButton } from '../Core/Button';
+import { FaCaretRight } from 'react-icons/fa';
+
 
 const Bg = styled.div`
   margin: 70px 0px 70px 0px; 
 `
 
-const DetailLeft = ({ subTitle,pathImg }) => {
+const Img = styled.img`
+  @media screen and (min-width: 320px) and (max-width: 375px){
+    width: 260px;
+  }
+  @media screen and (min-width: 375px) and (max-width: 425px){
+    width: 315px;
+  }
+  @media screen and (min-width: 376px) and (max-width: 769px) {
+    width: 350px;
+  }
+`
+
+const DetailLeft = ({ subTitle,pathImg,msgButton }) => {
   return (
     <Bg>
       <Container>
         <Row>
-          <Col lg={5} sm={12}>
+          <Col lg={5} md={6} sm={12} className="order-2">
             <SubTitle>
-              <strong>
-                <MDreact escapeHtml={false} source={subTitle}/>
-              </strong>
+              <MDreact escapeHtml={false} source={subTitle}/>
             </SubTitle>
+            { msgButton ? <PrimaryButton>
+              {msgButton}
+              <FaCaretRight/>
+            </PrimaryButton>: ''}
           </Col>
-          <Col lg={7} sm={12} className="d-flex justify-content-center">
-            <img src={pathImg} />
+          <Col lg={7} md={6} sm={12} className="d-flex justify-content-center order-md-2 oder-md-1">
+            <Img src={pathImg} />
           </Col>
         </Row>
       </Container>
