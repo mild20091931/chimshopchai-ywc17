1ef5a3609cb862c63e3e16e616a52c153b06d4db
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-01 01:42:03 +0700

    [Update] edit path apiService

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
-import React, { Component,Fragment } from 'react';
-import api from '../Core/apiService';
+import React, { Component, Fragment } from 'react';
+import api from '../../service/apiService';
 import Nav from '../Core/Nav';
 import Banner from '../Core/Banner';
 import RegisterDetail from './Register';
@@ -10,45 +10,56 @@ import Footer from './Footer';
 import Store from './DetailLeft';
 import Step from './DetailRight';
 
-
 const content = `<p><strong>สิทธิต่อที่ 1 : รับเงินสนับสนุน 1,000 บาท<br/>
 สิทธิต่อที่ 2 : รับเงินชดเชยคืน 15%-20% (สูงสุด 8,500 บาท)</strong></p>
 <p>แบ่งเป็น <br/>1. 15% สำหรับยอดใช้จ่ายสะสมไม่เกิน 30,000 บาท<br/>
-2. 20% สำหรับยอดใช้จ่ายสะสมส่วน 30,000 ถึง 50,000 บาท</p>`
+2. 20% สำหรับยอดใช้จ่ายสะสมส่วน 30,000 ถึง 50,000 บาท</p>`;
 
 class Index extends Component {
-  state  = {
+  state = {
     navbarItems: [],
     duration: '',
-    detail:'',
-    condition:'',
-  }
+    detail: '',
+    condition: '',
+  };
 
-  componentDidMount = async () =>{
+  componentDidMount = async () => {
     let data = await api.getData();
     this.handleData(data.data);
-  }
+  };
 
-  handleData = async (items) => {
+  handleData = async items => {
     this.setState({
-      navbarItems : items.navbarItems,
+      navbarItems: items.navbarItems,
       duration: items.duration,
       detail: items.detail,
-      condition: items.condition
-    })
-  }
+      condition: items.condition,
+    });
+  };
 
   render() {
     const { navbarItems, duration, detail, condition } = this.state;
     return (
       <Fragment>
-        <Nav navbarItems={navbarItems}/>
-        <Banner/>
-        <RegisterDetail duration={duration}/>
-        <BoxDetail detail={detail} condition={condition}/>
-        <Store subTitle="ค้นหารายชื่อร้านค้า" pathImg='img/banner_store.png' msgButton="รายชื่อร้านค้าที่เข้าร่วมโครงการ"/>
-        <Step subTitle="ขั้นตอนการเข้าใช้งานผ่าน<br>แอปฯ “เป๋าตัง”" video="https://www.youtube.com/embed/ZkniwQLv_Xk" msgButton="ขั้นตอนทั้งหมด"/>
-        <Store subTitle="เติมเงินเข้า “เป๋าตัง”<br/> (G-Wallet)" pathImg='img/banner_touup-GW.png' msgButton="ดูขั้นตอนทั้งหมด"/>
+        <Nav navbarItems={navbarItems} />
+        <Banner />
+        <RegisterDetail duration={duration} />
+        <BoxDetail detail={detail} condition={condition} />
+        <Store
+          subTitle="ค้นหารายชื่อร้านค้า"
+          pathImg="img/banner_store.png"
+          msgButton="รายชื่อร้านค้าที่เข้าร่วมโครงการ"
+        />
+        <Step
+          subTitle="ขั้นตอนการเข้าใช้งานผ่าน<br>แอปฯ “เป๋าตัง”"
+          video="https://www.youtube.com/embed/ZkniwQLv_Xk"
+          msgButton="ขั้นตอนทั้งหมด"
+        />
+        <Store
+          subTitle="เติมเงินเข้า “เป๋าตัง”<br/> (G-Wallet)"
+          pathImg="img/banner_touup-GW.png"
+          msgButton="ดูขั้นตอนทั้งหมด"
+        />
         <Step
           subTitle="ใช้จ่าย สะดวก ปลอดภัย <br/> “เป๋าตัง” (G-Wallet)"
           pathImg="img/download.png"
@@ -56,9 +67,9 @@ class Index extends Component {
           pic1="img/AppStore.png"
           pic2="img/GooglePlay.png"
         />
-        <BoxCall/>
-        <Brand/>
-        <Footer navbarItems={navbarItems}/>
+        <BoxCall />
+        <Brand />
+        <Footer navbarItems={navbarItems} />
       </Fragment>
     );
   }
