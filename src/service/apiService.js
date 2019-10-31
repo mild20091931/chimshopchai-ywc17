ac05b3dd3b75257ce076a99252fe9d45e5b628e8
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-01 01:41:33 +0700

    [Update] move apiService.js to service folder

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,10 +1,10 @@
-import api from "../../utils/api";
+import api from '../utils/api';
 
 const Api = {
   getData: async () => {
     let data = await api.get();
     return data;
-  }
-}
+  },
+};
 
 export default Api;
