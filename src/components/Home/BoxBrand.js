21c0146a4aa8eaf792ae66bfbebb26e3430a49fb
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-11-01 20:04:22 +0700

    [Update] hover band logo

--- src/components/Home/BoxBrand.js
+++ src/components/Home/BoxBrand.js
@@ -1,24 +1,30 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, NavLink } from 'reactstrap';
 import { BgBox } from '../Core/Bg';
 
 const Img = styled.img`
   width: 102px;
+  display: 'flex';
+  &:hover {
+    transform: scale(1.2);
+    transition: all 0.55s ease-in-out;
+  }
   @media screen and (min-width: 320px) and (max-width: 500px) {
     width: 56px;
   }
 `;
 
 const pathImg = [
-  { img: 'MOF.png', position: 'right' },
-  { img: 'FPO.png', position: 'center' },
-  { img: 'CGD.png', position: 'left' },
-  { img: 'Krungthai.png', position: 'right' },
-  { img: 'MOTS.png', position: 'center' },
-  { img: 'TAT.png', position: 'left' },
+  { img: 'MOF.png', position: 'right', link: 'https://www.mof.go.th/th/home' },
+  { img: 'FPO.png', position: 'center', link: 'http://www.fpo.go.th' },
+  { img: 'CGD.png', position: 'left', link: 'https://www.cgd.go.th' },
+  { img: 'Krungthai.png', position: 'right', link: 'https://www.newcb.ktb.co.th' },
+  { img: 'MOTS.png', position: 'center', link: 'https://www.mots.go.th' },
+  { img: 'TAT.png', position: 'left', link: 'https://thai.tourismthailand.org' },
 ];
+
 const BoxBrand = () => {
   return (
     <BgBox margin="150px 0px 150px 0px">
@@ -27,7 +33,9 @@ const BoxBrand = () => {
           {pathImg.map((data, i) => {
             return (
               <Col sm={4} key={i} className={`col-4 text-${data.position}`}>
-                <Img src={`img/brand_logo/${data.img}`} />
+                <NavLink href={data.link}>
+                  <Img src={`img/brand_logo/${data.img}`} />
+                </NavLink>
               </Col>
             );
           })}
@@ -39,7 +47,9 @@ const BoxBrand = () => {
             {pathImg.map((data, i) => {
               return (
                 <Col md={2} lg={2} key={i} className="d-flex justify-content-center">
-                  <Img src={`img/brand_logo/${data.img}`} />
+                  <NavLink href={data.link}>
+                    <Img src={`img/brand_logo/${data.img}`} />
+                  </NavLink>
                 </Col>
               );
             })}
